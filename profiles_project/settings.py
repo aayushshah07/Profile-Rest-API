35c449ca12fab1880086988a6b9ed88125651741
aayushshah07 <aayushshah1954@gmail.com>
2022-01-31 23:17:47 +0530

    added custom user profile and superuser migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framewok',
-    # 'rest_framework.authtoken',
+    # 'rest_framewok'.authtoken',
     'profiles_api',
 ]
 
@@ -121,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'profiles_api.Userprofile'
