ce3481c92897713674e769c0bcd12450720623d2
aayushshah07 <aayushshah1954@gmail.com>
2022-02-06 16:50:32 +0530

    Hello-ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    # Test Api view
+
+    def get(self,req,format=None):
+        an_apiview = [
+            'Aayush',
+            'Shah',
+            'SDE at Amazon',
+        ]
+
+        return Response({'message' : 'Hello', 'an_apiview':an_apiview})
+
+     
